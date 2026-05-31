bad453deb948a025d3acc1da5558410de05f4413
Emi Hayakawa <52098983+hayamin1111@users.noreply.github.com>
2026-05-31 12:50:44 +0900

    feat: state追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,122 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState, useEffect } from 'react';
+import type { Screening } from './types/screenings';
+import './App.css';
+
+/**
+ * json取得し使えるデータとして返す関数
+ */
+const fetchScreenings = async (): Promise<Screening[]> => {  
+  const response = await fetch('./screenings.json');
+  
+  if (!response.ok) {
+    throw new Error("データ取得に失敗しました");
+  }
+
+  const json = await response.json();
+  
+  if (json.length === 0) {
+    throw new Error('データがありません');
+  }
+  
+  return json;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [screenings, setScreenings] = useState<Screening[]>([]);
+
+  /**
+   * 初回表示時にfetchScreeningsを呼んで、stateに入れる関数
+   */
+  useEffect(() => {
+    const init = async () => {
+      try {
+        const data = await fetchScreenings();
+        setScreenings(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    init();
+  }, [])
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+     <p>テスト</p>
+      {
+        
+        screenings.map((screening) => (
+          <div key={screening.id}>
+            <p>{screening.title}</p>
+            <p>{screening.date}</p>
+          </div>
+        ))
+      }
     </>
   )
 }
 
-export default App
+export default App;
